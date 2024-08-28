977c7540385c12e7d27cd58c09983358b1406c1b
SoumyaOk4-4 <soumya.chk2004@gmail.com>
2024-08-28 20:43:18 +0530

    initial stage

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <a href="http://equisource.duckdns.org:7099/">Get Started</a>
+    </div>
+  );
+}
+
+export default App;
